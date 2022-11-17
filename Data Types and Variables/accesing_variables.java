08e2d1f0c95269d5670c021fe609d4166f1096fb
Naveen Kumar N P <73624481+naveen648@users.noreply.github.com>
2022-11-17 12:57:44 +0530

    Create accesing_variables.java

---
+++ Data Types and Variables/accesing_variables.java
@@ -0,0 +1,17 @@
+class acess
+            {
+                int x;
+                int y;
+            }
+public class accesing_variables 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("hello");
+        acess a=new acess();
+        a.x=10;
+        a.y=20;
+        System.out.println(a.x+" "+a.y);
+    }
+    
+}
