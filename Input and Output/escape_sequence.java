6f986eacd7fd0915bc07cb925119fa8a9929721e
Naveen Kumar N P <73624481+naveen648@users.noreply.github.com>
2022-11-21 19:43:21 +0530

    Add files via upload

---
+++ Input and Output/escape_sequence.java
@@ -0,0 +1,9 @@
+public class escape_sequence {
+    public static void main(String[] args)
+    {
+        //to print inverted comma's for specific output statement by using \"Type Here \"
+        System.out.println("hi i am coding \"JAVA\"");
+        //to print windows path directory by using "\\"
+        System.out.println("C:\\xyz\\abc.txt");
+    }
+}
