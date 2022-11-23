e12251e6289085cea258f71d5d6c10def4186a00
Naveen Kumar N P <73624481+naveen648@users.noreply.github.com>
2022-11-23 19:08:53 +0530

    Create shift_operators.java

---
+++ operators/shift_operators.java
@@ -0,0 +1,15 @@
+public class shift_operators 
+{
+    public static void main(String[] args)
+    {
+        int x=33,y=-2;
+        //Signed Right Shift Operators
+        System.out.println(x>>1);
+        System.out.println(x>>2);
+        System.out.println(y>>1);
+        System.out.println(y>>2);
+        //Unsigned Right Shift Operators
+        System.out.println(y>>>1);
+        System.out.println(y>>>2);
+    }
+}
