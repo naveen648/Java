4a80dd80ee77a49a72a7cab251ec16b5052c30de
Naveen Kumar N P <73624481+naveen648@users.noreply.github.com>
2022-11-27 21:31:30 +0530

    Update if_else_natural_numbers.java

--- Flow Control/if_else_natural_numbers.java
+++ Flow Control/if_else_natural_numbers.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
+public class if_else_natural_numbers {
+    public static void main(String[] args)
+    {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the Number to print Natural Numbers");
+    int input=sc.nextInt();
+    if(input<0)
+    {
+        System.out.println("Invalid Input");
+        return ;
+    }
+        System.out.println("Natural Numbers for "+input+" = "+input*(input+1)/2);
+    }
+}
 
