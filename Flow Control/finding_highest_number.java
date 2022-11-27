177393839e5774e9f94c216b72617b230b8fc230
Naveen Kumar N P <73624481+naveen648@users.noreply.github.com>
2022-11-27 21:36:06 +0530

    Create finding_highest_number.java

---
+++ Flow Control/finding_highest_number.java
@@ -0,0 +1,10 @@
+public class finding_highest_number 
+{
+    public static void main(String[] args)
+    {
+        int a=10,b=30,c=20;
+        int d=Math.max(a, b);
+        int ans=Math.max(c, d);
+         System.out.println("largest of three numbers is = "+ans );
+    }
+}
