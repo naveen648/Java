57f56ce13ee0c9ec50750c15728d19e13652121d
Naveen Kumar N P <73624481+naveen648@users.noreply.github.com>
2022-11-29 07:07:12 +0530

    Create string.java

---
+++ trings/string.java
@@ -0,0 +1,9 @@
+public class string {
+    public static void main(String[] args)
+    {
+        String str="hello";
+        System.out.println(str.length());
+        System.out.println(str.charAt(3));
+        System.out.println(str.substring(1,5));
+    }
+}
