460105da558c82421326dd5c7adb4b0a8fae5ae6
Naveen Kumar N P <73624481+naveen648@users.noreply.github.com>
2022-11-30 11:30:09 +0530

    Create contains_function.java

---
+++ Strings/contains_function.java
@@ -0,0 +1,10 @@
+public class contains_function {
+    public static void main(String[] args)
+    {
+        String s1="geeksforgeeks";
+        String s2="for";
+        String s3="geeks";
+        System.out.println(s1.contains(s2));
+        System.out.println(s1.contains(s3));
+    }
+}
