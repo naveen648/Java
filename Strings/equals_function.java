eb8b4689f73daddd65008666df455ef5f7d0d785
Naveen Kumar N P <73624481+naveen648@users.noreply.github.com>
2022-11-30 11:30:45 +0530

    Create equals_function.java

---
+++ Strings/equals_function.java
@@ -0,0 +1,10 @@
+public class equals_function {
+    public static void main(String[] args)
+    {
+        String s1="geeksforgeeks";
+        String s2="geeksforgeeks";
+        String s3="geeks";
+        System.out.println("S1 = S2 "+s1.equals(s2));
+        System.out.println("S1 = S3 "+s1.equals(s3));
+    }
+}
