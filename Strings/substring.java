e7e73f5bd3d480c162f5a1f8e02e9a53bca4d21b
Naveen Kumar N P <73624481+naveen648@users.noreply.github.com>
2022-11-30 15:05:34 +0530

    Create substring.java

---
+++ Strings/substring.java
@@ -0,0 +1,9 @@
+public class substring {
+    public static void main(String[] args)
+    {
+        String str ="geeksforgeeks";
+        System.out.println(str.substring(1));
+        System.out.println(str.substring(2,7));
+        System.out.println(str.substring(0,13));
+    }
+}
