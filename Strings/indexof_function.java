f856b9f8f2583bfed5cac83023fe5efcbf93a57e
Naveen Kumar N P <73624481+naveen648@users.noreply.github.com>
2022-11-30 11:32:10 +0530

    Create indexof_function.java

---
+++ Strings/indexof_function.java
@@ -0,0 +1,11 @@
+public class indexof_function {
+    public static void main(String[] args)
+    {
+        String s1="geeksforgeeks";
+        String s2="geeks";
+        String s3="for";
+        System.out.println(s1.indexOf(s2));
+        System.out.println(s1.indexOf(s2,1));
+        System.out.println(s1.indexOf(s3));
+    }
+}
