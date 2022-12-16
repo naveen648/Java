52d353c6999075d6058887d89848182f775963f7
Naveen Kumar N P <73624481+naveen648@users.noreply.github.com>
2022-12-16 21:05:00 +0530

    Create TraversalUsingGet2.java

---
+++ ArrayLists/TraversalUsingGet2.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+public class TraversalUsingGet2 {
+    public static void main(String[] args)
+    {
+        ArrayList<String> al=new ArrayList<String>();
+        al.add("Java");
+        al.add("Coding");
+        al.add("Courses");
+        for(String s:al)
+        {
+            System.out.println(s);
+        }
+    }
+}
