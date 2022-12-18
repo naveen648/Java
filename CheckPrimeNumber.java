29b3b616561a68f8b6a083a60c2daaae3754c918
Naveen Kumar N P <73624481+naveen648@users.noreply.github.com>
2022-12-19 00:11:07 +0530

    Create CheckPrimeNumber.java

---
+++ CheckPrimeNumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class CheckPrimeNumber
+{
+    public static void main(String[] args)
+    {
+        for(;;)
+        {
+        System.out.println("Enter the Number to check Prime or Non-Prime");
+        try(Scanner sc=new Scanner(System.in))
+        {
+        int p=sc.nextInt();
+        
+        if(p%2==1&&p%3==2)
+        {
+            System.out.println(p+ " = Prime No. ");
+        }
+        else if(p==1||p==2)
+        {
+            System.out.println("1 = Prime No.");
+        }
+        else
+        {
+            System.out.println(p+ " = Non-Prime No.");
+        }
+        }
+        }
+    }
+}
