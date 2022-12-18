984c26e9cb74027999fba5235d20bd3402eaf186
Naveen Kumar N P <73624481+naveen648@users.noreply.github.com>
2022-12-19 02:52:25 +0530

    Create DoWhile .java

---
+++ Loops/DoWhile .java
@@ -0,0 +1,14 @@
+public class DoWhile 
+{
+    public static void main(String[] args)
+    {
+        int i=0;
+        do
+        {
+            System.out.println("Hello");
+            i++;
+        }while(i<0);            
+        /*prints the statements inside the loop atleast once even if condition is not satisfied 
+        and then checks Condition*/
+    }
+}
