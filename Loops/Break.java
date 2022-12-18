7b5db941cf034f09031180ed2f4732fcca958816
Naveen Kumar N P <73624481+naveen648@users.noreply.github.com>
2022-12-19 02:47:10 +0530

    Create Break.java

---
+++ Loops/Break.java
@@ -0,0 +1,11 @@
+public class Break {
+    public static void main(String[] args)
+    {
+        for(int i=0;i<6;i++)
+        {
+            if(i==4)
+                break;
+            System.out.print(i+" ");
+        }
+    }
+}
