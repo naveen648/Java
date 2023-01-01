1ff39001cc9f04e1c9ead04531df5a6e1fe94e88
Naveen Kumar N P <73624481+naveen648@users.noreply.github.com>
2023-01-01 12:18:21 +0530

    Create factorial_biginteger.java

---
+++ Advanced/factorial_biginteger.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+//import java.util.*;
+public class factorial_biginteger {
+    public static BigInteger factorial(int a)
+    {
+        BigInteger C=new BigInteger("1");
+        for(int i=2;i<=a;i++)
+        {
+            BigInteger D = BigInteger.valueOf(i);
+            C=C.multiply(D);
+        }
+        return C;
+    }
+    public static void main(String[] args)
+    {
+        try (Scanner sc = new Scanner(System.in)) {
+            int a=sc.nextInt();
+            //int b=factorial(a);
+            System.out.println(factorial(a));
+        }
+    }
+    
+}
