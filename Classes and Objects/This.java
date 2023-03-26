d9d497bb9644cd459c2a5f6c42dae2963a587a8c
Naveen Kumar N P <73624481+naveen648@users.noreply.github.com>
2023-03-26 11:30:35 +0530

    Create This.java

---
+++ Classes and Objects/This.java
@@ -0,0 +1,21 @@
+class ThisKeyword {
+    public static void main(String[] args) 
+    {
+        A obj1=new A(10,20);
+        A obj2=new A(50,100);
+        obj1.print();
+        obj2.print();
+    }
+}
+class A{
+    int x,y;  
+    A(int x,int y)  //Overrides the scope of x and y values and no need to declare new variables
+    {
+        this.x=x;
+        this.y=y;
+    }
+    void print()
+    {
+        System.out.println("X = "+x+"\tY = "+y);
+    }
+}
