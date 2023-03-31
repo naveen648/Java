157803f9201c910e9bebbb02361134c657501bf0
Naveen Kumar N P <73624481+naveen648@users.noreply.github.com>
2023-03-31 17:18:38 +0530

    Create PositiveNegative.java

---
+++ Code Snippets/PositiveNegative.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class PositiveNegative{
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    if(n<1)
+    {
+      System.out.print("The number is positive.");
+    }
+    else
+    {
+      System.out.print("The number is negative.");
+    }
+  }
+}
