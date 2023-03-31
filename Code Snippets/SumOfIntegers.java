a71a2923dddd9325e9561796b1bbb842ef11d428
Naveen Kumar N P <73624481+naveen648@users.noreply.github.com>
2023-03-31 17:32:13 +0530

    Create SumOfIntegers.java

---
+++ Code Snippets/SumOfIntegers.java
@@ -0,0 +1,11 @@
+class SumOfIntegers{
+  public static void main(String[] args)
+  {
+    int x=10,y=30;
+    System.out.print(sum(x,y));
+  }
+  public static int sum(int x, int y)
+  {
+    return x+y;
+  }
+}
